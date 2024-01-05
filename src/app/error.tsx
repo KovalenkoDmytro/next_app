c85ad16e2fff93485c92c36025850933c4e8e98b
Dmytro Kovalenko <76845911+DmytroKalenko@users.noreply.github.com>
2024-01-04 19:02:40 -0700

    refactor: Made global error handling

---
+++ src/app/error.tsx
@@ -0,0 +1,21 @@
+'use client'
+ 
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  return (
+    <html>
+      <body>
+        <div>
+          <h2>{error.message}</h2>
+          <p>Something went wrong! Error page </p>
+          <button onClick={() => reset()}>Try again</button>
+        </div>
+      </body>
+    </html>
+  )
+}
